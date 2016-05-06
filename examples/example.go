bc2af9b26fb532413061d6f4433b65d0f3d19992
Hamish <hamish.ogilvy@gmail.com>
2016-05-06 16:33:24 +1000

    Fix example for Travis

--- examples/example.go
+++ examples/example.go
@@ -3,20 +3,32 @@ package main
 import "github.com/sajari/regression"
 
 func main() {
-	var r regression.Regression
-	r.SetObservedName("Z")
-	r.SetVarName(0, "A")
-	r.SetVarName(1, "B")
-	r.SetVarName(2, "C")
-	// Z = 12 + 1A + 2B +3C
-	r.AddDataPoint(regression.DataPoint{Observed: 12, Variables: []float64{0, 0, 0}})
-	r.AddDataPoint(regression.DataPoint{Observed: 13, Variables: []float64{1, 0, 0}})
-	r.AddDataPoint(regression.DataPoint{Observed: 14, Variables: []float64{0, 1, 0}})
-	r.AddDataPoint(regression.DataPoint{Observed: 15, Variables: []float64{0, 0, 1}})
-	r.AddDataPoint(regression.DataPoint{Observed: 14, Variables: []float64{2, 0, 0}})
-	r.AddDataPoint(regression.DataPoint{Observed: 16, Variables: []float64{0, 2, 0}})
-	r.AddDataPoint(regression.DataPoint{Observed: 18, Variables: []float64{0, 0, 2}})
-
-	r.RunLinearRegression()
-	r.Dump(true)
+	r := new(Regression)
+	r.SetObserved("Murders per annum per 1,000,000 inhabitants")
+	r.SetVar(0, "Inhabitants")
+	r.SetVar(1, "Percent with incomes below $5000")
+	r.SetVar(2, "Percent unemployed")
+	r.Train(
+		DataPoint(11.2, []float64{587000, 16.5, 6.2}),
+		DataPoint(13.4, []float64{643000, 20.5, 6.4}),
+		DataPoint(40.7, []float64{635000, 26.3, 9.3}),
+		DataPoint(5.3, []float64{692000, 16.5, 5.3}),
+		DataPoint(24.8, []float64{1248000, 19.2, 7.3}),
+		DataPoint(12.7, []float64{643000, 16.5, 5.9}),
+		DataPoint(20.9, []float64{1964000, 20.2, 6.4}),
+		DataPoint(35.7, []float64{1531000, 21.3, 7.6}),
+		DataPoint(8.7, []float64{713000, 17.2, 4.9}),
+		DataPoint(9.6, []float64{749000, 14.3, 6.4}),
+		DataPoint(14.5, []float64{7895000, 18.1, 6}),
+		DataPoint(26.9, []float64{762000, 23.1, 7.4}),
+		DataPoint(15.7, []float64{2793000, 19.1, 5.8}),
+		DataPoint(36.2, []float64{741000, 24.7, 8.6}),
+		DataPoint(18.1, []float64{625000, 18.6, 6.5}),
+		DataPoint(28.9, []float64{854000, 24.9, 8.3}),
+		DataPoint(14.9, []float64{716000, 17.9, 6.7}),
+		DataPoint(25.8, []float64{921000, 22.4, 8.6}),
+		DataPoint(21.7, []float64{595000, 20.2, 8.4}),
+		DataPoint(25.7, []float64{3353000, 16.9, 6.7}),
+	)
+	r.Run()
 }
